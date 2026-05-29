5aeb9491580c2e20a0ae884ee3f7fe1871caa330
Ankit Kumar <Ankit27.iitd@gmail.com>
2026-05-29 17:32:27 +0530

    started with 1400, wish me luck

---
+++ 1400/E_Anna_and_the_Valentine_s_Day_Gift.cpp
@@ -0,0 +1,37 @@
+/*
+    ./execute  [E_Anna_and_the_Valentine_s_Day_Gift.cpp]                     
+    * author    : two-seven                                         
+    * created   : 17:30:09 on 29-May-2026, Friday
+    * path      : C:\Users\Ankit\Desktop\Placement Preparation\Cp31 sheet\1400                                   
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+// ---- type definitions and macros
+#define int long long
+using ll = long long;
+using vi = vector<int>;
+using vpi = vector<pair<int, int>>;
+using pi = pair<int, int>;
+using vvi = vector<vector<int>>;
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define yn(a) cout << (a ? "YES\n" : "NO\n")
+#define pb push_back
+
+// ---- constants 
+constexpr ll inf = 1e15;
+constexpr ll mod = 1e9 + 7;
+//constexpr ll mod = 998244353;
+
+void solve() {
+    
+}
+
+signed main() {
+    fastio;
+    // solve(); // Uncomment for single test case 
+    int tc; cin >> tc; while(tc--) {solve();} //  Multiple test cases 
+    return 0;
+}
